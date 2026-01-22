f7b275b371b336042638db3aba03254ba0540144
Batiste0912 <batiste.dubo@outlook.fr>
2026-01-22 16:29:09 +0800

    :sparkles: Add database connection and initial SQL schema

---
+++ configuration/database.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config();
+
+const database = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'palu',
+})
+
+export default database;
